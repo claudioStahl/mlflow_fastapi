7924e86ecedc28e5298d9e3693dcd2265cc74c17
claudioStahl <claudioarosseto@gmail.com>
2021-08-29 16:22:03 -0300

    improve prepare model command

--- mlflow_fastapi/prepare_model.py
+++ mlflow_fastapi/prepare_model.py
@@ -6,14 +6,19 @@ from mlflow.tracking.artifact_utils import _download_artifact_from_uri
 
 _logger = logging.getLogger(__name__)
 
-# model_uri = "models:/ElasticnetWineModel/Staging"
+model_path = os.getcwd() + "/model"
+tmp_model_path = os.getcwd() + "/tmp/model_tmp"
+trash_model_path = os.getcwd() + "/tmp/model_trash"
 
 
 def do_prepare_model(model_uri):
-    model_path = os.getcwd() + "/model"
+    local_path = _download_artifact_from_uri(model_uri)
+    shutil.copytree(local_path, tmp_model_path)
 
     if os.path.exists(model_path):
-        shutil.rmtree(model_path)
+        os.rename(model_path, trash_model_path)
+    
+    os.rename(tmp_model_path, model_path)
 
-    local_path = _download_artifact_from_uri(model_uri)
-    shutil.copytree(local_path, model_path)
+    if os.path.exists(trash_model_path):
+        shutil.rmtree(trash_model_path)
